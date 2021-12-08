f4ab5997929bc6050ac6eb4f1ac1e0dfe952e337
Bruno Bergamaschi <brunobergamaschi@live.com>
2021-12-08 09:38:14 -0300

    correção update - remover arquivo atual e manter apenas o útimo upado

--- Rentx/src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ Rentx/src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -1,5 +1,6 @@
 import "reflect-metadata";
 import { inject, injectable } from "tsyringe";
+import { deleteFile } from "../../../../utils/file";
 import { IUsersRepository } from "../../respositories/IUsersRepository";
 
 interface IRequest {
@@ -22,6 +23,9 @@ class UpdateUserAvatarUseCase {
 
 	async execute({ user_id, avatar_file }: IRequest): Promise<void> {
 		const user = await this.userRepository.findById(user_id);
+
+		await deleteFile(`./tmp/avatar/${user.avatar}`);
+
 		user.avatar = avatar_file;
 
 		await this.userRepository.create(user);
